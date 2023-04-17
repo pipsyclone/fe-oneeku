18da8a521220c9e90c77e9ef46d22b09d1817032
ACER <apipska.10@gmail.com>
2023-04-17 08:56:06 +0700

    Error Handling Updated

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 const ENV = () => {
-    const productionURL     = "https://myoneekuserver.vercel.app";
+    const productionURL     = "https://oneekuserver.vercel.app";
     const developmentURL    = "http://localhost:5000";
     const baseURL_API       = productionURL;
 
